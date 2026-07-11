f46f21bcc632b32ba1ed0844b66884353e99772c
hirosof <for-git@mail.hirosof.dev>
2026-07-11 19:02:08 +0900

    [BitNumberCpp] main.cppの充実化を実施

--- BitNumberCpp/BitNumberCpp/main.cpp
+++ BitNumberCpp/BitNumberCpp/main.cpp
@@ -1,23 +1,114 @@
 #define NOMINMAX
 #include <windows.h>
 #include <locale>
+#include <vector>
+#include<random>
 #include "CUnsignedBitNumber.hpp"
 
-void func1( void );
-void uselevel_from_test_func( void );
-void arithmeticTest( void );
+enum struct NumberType {
+	Binary = 0,
+	Decimal,
+	Hexadecimal
+};
 
-template<size_t BitSize, typename BitNumberDefaultCharT> void print_bitNumberWBS( std::string prefix, const CUnsignedBitNumber<BitSize, BitNumberDefaultCharT>& num, bool withNewLine = true ) {
+template <size_t BitSize>  void arithmeticSample( const std::string number1String, const std::string number2String, const NumberType numType = NumberType::Hexadecimal ) {
 
-	if ( prefix.length( ) != 0 ) printf( "%s", prefix.c_str( ) );
 
-	printf( " (%zu Bit Type) : %s", BitSize  , num.toJsonLikedString<char>().c_str());
+	CUnsignedBitNumberA<BitSize>  number1, number2;
 
-	if ( withNewLine ) printf( "\n" );
+
+	switch ( numType ) {
+		case NumberType::Binary:
+			number1.fromBinaryString( number1String );
+			number2.fromBinaryString( number2String );
+			break;
+		case NumberType::Decimal:
+			number1.fromDecimalString( number1String );
+			number2.fromDecimalString( number2String );
+			break;
+		case NumberType::Hexadecimal:
+			number1.fromHexadecimalString( number1String );
+			number2.fromHexadecimalString( number2String );
+			break;
+	}
+
+	arithmeticSample( number1, number2 );	
 }
 
 
 
+template <size_t BitSize>  void arithmeticSample( const CUnsignedBitNumberA<BitSize>& number1, const CUnsignedBitNumberA<BitSize>& number2 ) {
+	using Pair = std::pair<std::string, CUnsignedBitNumberA<BitSize>>;
+	using PairVector = std::vector<Pair>;
+
+	PairVector result_items;
+
+	result_items.push_back( Pair( "number1", number1 ) );
+	result_items.push_back( Pair( "number2", number2 ) );
+	result_items.push_back( Pair( "number1 + number2", number1 + number2 ) );
+	result_items.push_back( Pair( "number1 - number2", number1 - number2 ) );
+	result_items.push_back( Pair( "number2 - number1", number2 - number1 ) );
+
+	result_items.push_back( Pair( "number1 * number2", number1 * number2 ) );
+
+
+	auto temp_div_remain = number1.divisionWithRemainder( number2 );
+	if ( temp_div_remain.has_value( ) ) {
+		result_items.push_back( Pair( "number1 / number2", temp_div_remain->first ) );
+		result_items.push_back( Pair( "number1 % number2", temp_div_remain->second ) );
+	}
+
+	temp_div_remain = number2.divisionWithRemainder( number1 );
+	if ( temp_div_remain.has_value( ) ) {
+		result_items.push_back( Pair( "number2 / number1", temp_div_remain->first ) );
+		result_items.push_back( Pair( "number2 % number1", temp_div_remain->second ) );
+	}
+
+	size_t  num_of_result_items = result_items.size( );
+
+	if ( num_of_result_items > 0 ) {
+
+		printf( "```json\n{\n" );
+
+		printf( "\t\"BitSize\":\"%zu\",\n", BitSize );
+		for ( size_t i = 0; i < num_of_result_items; i++ ) {
+
+			printf( "\t\"%s\":%s", result_items[i].first.c_str( ), result_items[i].second.toJsonLikedString<char>( true ).c_str( ) );
+
+			if ( ( i + 1 ) != num_of_result_items ) {
+				printf( "," );
+			}
+			printf( "\n" );
+		}
+		printf( "}\n```\n\n" );
+	}
+}
+
+template <size_t BitSize>  void arithmeticSampleRandom( void ) {
+
+	CUnsignedBitNumberA<BitSize>  number1, number2;
+
+	std::random_device rd;
+	std::mt19937 re( rd() );
+
+	std::uniform_int_distribution<int>   dist( 0, 1 );
+
+
+	// number1 のセットするビット数は BitSize固定で発行
+	for ( size_t i = 0; i < BitSize; i++ ) {
+		number1.raw[i] = ( dist( re ) == 1 );
+	}
+
+	// number2 のセットするビット数は 可変で発行
+	std::uniform_int_distribution<int>   dist_n2_set_size( BitSize / 2, BitSize );
+	size_t size2 = dist_n2_set_size( re );
+	for ( size_t i = 0; i < size2; i++ ) {
+		number2.raw[i] = ( dist( re ) == 1 );
+	}
+
+	arithmeticSample( number1, number2 );
+
+}
 
 int main( ) {
 
@@ -27,105 +118,35 @@ int main( ) {
 	// UTF-8コードページに変更
 	SetConsoleOutputCP( CP_UTF8 );
 
-	/*
-	CUnsignedBitNumber16W  base = CUnsignedBitNumber16W::FromHelpers<char>::FromHexadecimalString( "C9DB" );
-	CUnsignedBitNumber128 large( base, 60, 0 );
 
-	print_bitNumberWBS( "base", base );
-	print_bitNumberWBS( "large", large );
-	print_bitNumberWBS( "large-base", large.subtraction(base.toCast<128,char>() ));
+	printf( "### 8 bit\n\n" );
+	arithmeticSampleRandom<8>( );
+	arithmeticSampleRandom<8>( );
 
+	printf( "### 16 bit\n\n" );
+	arithmeticSampleRandom<16>( );
+	arithmeticSampleRandom<16>( );
 
-	CUnsignedBitNumber16W b2;
+	printf( "### 24 bit\n\n" );
+	arithmeticSampleRandom<24>( );
+	arithmeticSampleRandom<24>( );
 
-	b2.fromCast( base, 0, 8 );
+	printf( "### 32 bit\n\n" );
+	arithmeticSampleRandom<32>( );
+	arithmeticSampleRandom<32>( );
 
-	print_bitNumberWBS( "b2", b2 );
-	*/
-	arithmeticTest( );
+	printf( "### 64 bit\n\n" );
+	arithmeticSampleRandom<64>( );
+	arithmeticSampleRandom<64>( );
+
+	printf( "### 128 bit\n\n" );
+	arithmeticSampleRandom<128>( );
+	arithmeticSampleRandom<128>( );
+
 
 	return 0;
 }
 
 
-void arithmeticTest( void ) {
 
 
-	bool separate = true;
-
-	CUnsignedBitNumber64 a = CUnsignedBitNumber64::FromHelpers<char>::FromHexadecimalString( "FB21 EF59" );
-	CUnsignedBitNumber64 b = CUnsignedBitNumber64::FromHelpers<char>::FromHexadecimalString( "843E CD60" );
-
-	printf( "a : %s\n", a.toJsonLikedString<char>( separate ).c_str( ) );
-	printf( "b : %s\n", b.toJsonLikedString<char>( separate ).c_str( ) );
-	printf( "\n" );
-	printf( "a+b : %s\n", a.addition( b ).toJsonLikedString<char>( separate ).c_str( ) );
-	printf( "a-b : %s\n", a.subtraction( b ).toJsonLikedString<char>( separate ).c_str( ) );
-	printf( "a*b : %s\n", a.multiplication( b ).toJsonLikedString<char>( separate ).c_str( ) );
-
-	auto div_rem = a.divisionWithRemainder( b );
-	if ( div_rem.has_value( ) ) {
-		printf( "a/b : %s\n", div_rem->first.toJsonLikedString<char>( separate ).c_str( ) );
-		printf( "a%%b : %s\n", div_rem->second.toJsonLikedString<char>( separate ).c_str( ) );
-	}
-
-}
-
-void uselevel_from_test_func( void ) {
-
-	CUnsignedBitNumber64 n64;
-	n64.fill( );
-
-	printf( "%I64u : %s\n\n", n64.toUInt64( ), n64.toJsonLikedString<char>( ).c_str( ) );
-
-
-	uint32_t  iv = 0x86214573;
-	CUnsignedBitNumber<32> v;
-
-	for ( size_t offset = 0; offset < 32; offset++ ) {
-		v.fromUInt32( iv, offset );
-		printf( "%u : ", iv << offset );
-		printf( "%s\n", v.toJsonLikedString<char>( ).c_str( ) );
-	}
-
-
-	printf( "-------------------------------\n" );
-
-	CUnsignedBitNumber<16> v16;
-	for ( size_t offset = 0; offset < 16; offset++ ) {
-		v16.fromUInt32( iv, offset );
-		printf( "%u : ", static_cast<uint16_t>( iv << offset ) );
-		printf( "%s\n", v16.toJsonLikedString<char>( ).c_str( ) );
-	}
-
-
-}
-
-
-void func1( void ) {
-
-	const int bitSize = 32;
-
-	std::bitset<bitSize> a = CStdBitsetUnsignedStringConversion<wchar_t>::FromHexadecimalString<bitSize>( L"1000ADD0", { ' ' } );
-	std::bitset<bitSize> b = CStdBitsetUnsignedStringConversion<char>::FromHexadecimalString<bitSize>( "110", { ' ' } );
-
-	auto dc = CStdBitsetUnsignedOperation::DivisionWithRemainder( a, b );
-
-	printf( "%u ÷ %u = ", a.to_ulong( ), b.to_ulong( ) );
-
-	if ( dc.has_value( ) ) {
-
-		printf( "%u あまり %u\n", dc->first.to_ulong( ), dc->second.to_ulong( ) );
-	} else {
-		printf( "なし\n" );
-	}
-
-
-	printf( "%s\n", CStdBitsetUnsignedStringConversion<char>::ToHexadecimalString( a ).c_str( ) );
-	printf( "%S\n", CStdBitsetUnsignedStringConversion<wchar_t>::ToHexadecimalString( b ).c_str( ) );
-
-	std::wstring w( { 'a'  , 'B' , 0 } );
-
-
-	printf( "%S\n", w.c_str( ) );
-}
